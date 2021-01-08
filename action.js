4e5693b543627e6ec888d9367009883bda8396df
gerald <geraldandriot@yahoo.fr>
2021-01-08 11:07:30 +0100

    bug

--- action.js
+++ action.js
@@ -1,4 +1,4 @@
-/*declenchement a la fin de la lecture du css sino le js ce lance tout de suite et il fait une erreur */
+/*declenchement a la fin de la lecture du css sinon le js ce lance tout de suite et il fait une erreur */
 window.onload = function() {
 
 //creation variables
@@ -17,7 +17,7 @@ function accueil(){
 
 //donne lemplacement ou il doit scroller pour projet
 function projet(){
-    window.scrollTo({top: 0, behavior: 'smooth'});
+    window.scrollTo({top: 420, behavior: 'smooth'});
 }
 
 //donne l'emplacement ou il doit scroller pour competences
@@ -29,9 +29,46 @@ function competences(){
 function contacts(){
     contact.scrollIntoView({behavior: 'smooth'});
 }
+
 // commande du clic
 nav.addEventListener("click" , accueil);
 nav1.addEventListener("click" , projet);
 nav2.addEventListener("click" , competences);
 nav3.addEventListener("click" , contacts);
-} 
+
+
+
+
+
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: .1
+}
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    console.log(entry.intersectionRatio)
+  })
+
+
+const observer = new IntersectionObserver(handleIntersect, options)
+observer.observe(document.querySelector('.reveal'))
+
+} /* <- rien après */
+/*
+// apparition des ellements
+$(window).scroll(function(){
+  var scrolledFromTop = $(window).scrollTop() + $(window).height();
+  $("info").each(function(){
+    var distanceFromTop = $(this).offset().top;
+    if(scrolledFromTop >= distanceFromTop+200){
+      console.log("hello");
+      var delaiAnim = $(this).data("delai");
+      $(this).delay(delaiAnim).animate({
+        opacity:1
+      });
+    }
+  });
+});
+*/
